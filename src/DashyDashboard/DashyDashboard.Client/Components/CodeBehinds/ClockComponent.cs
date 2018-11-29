29700667c971d215da0d8d9ffd23f25a61a8da47
Stig Voss <stig@stigvoss.dk>
2018-11-29 19:27:42 +0100

    Initial code commit

---
+++ src/DashyDashboard/DashyDashboard.Client/Components/CodeBehinds/ClockComponent.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashyDashboard.Client.Components.CodeBehinds
+{
+    public class ClockComponent : BlazorComponent
+    {
+        protected DateTime CurrentDateTime { get; private set; } = DateTime.Now;
+
+        protected string TimeFormat
+            => $"HH:mm{(ShowSeconds ? ":ss" : string.Empty)}";
+
+        protected string DateFormat
+            => "dddd, dd MMMM yyyy";
+
+        [Parameter]
+        protected bool ShowSeconds { get; set; }
+
+        protected override void OnInit()
+        {
+            StartUpdatingPeriodically();
+        }
+
+        private void StartUpdatingPeriodically()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    UpdateCurrentDateTime();
+                    StateHasChanged();
+
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        private void UpdateCurrentDateTime()
+        {
+            CurrentDateTime = DateTime.Now;
+        }
+    }
+}
---
+++ src/DashyDashboard/DashyDashboard.Client/Components/CodeBehinds/WeatherComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashyDashboard.Client.Components.CodeBehinds
+{
+    public class WeatherComponent : BlazorComponent
+    {
+    }
+}
